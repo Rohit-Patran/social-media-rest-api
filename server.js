637795c7d41229c44812be1db5f89b65c2d1d5d1
Rohit Patran <90060873+Rohit-Patran@users.noreply.github.com>
2023-04-18 00:26:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 
 app.get("/" , (request , response) => {
-  response.send("<center><h1>Welcome to Social media REST API</h1> <br> <p>Try out the mentioned routes using <strong>POSTMAN</strong> to the above URL</p> <br> <p>For more info visit this github repo --> <a href="https://github.com/Rohit-Patran/social-media-rest-api" target="_blank">link</a></p> </center>");
+    response.send("<center><h1>Welcome to Social media REST API</h1><br><p>Try out the mentioned routes using <strong>POSTMAN</strong> to the above URL</p><br><p>For more info visit this github repo --> <a href='https://github.com/Rohit-Patran/social-media-rest-api' target='_blank'>link</a></p></center>");
 });
 app.use("/api" , router);
 
